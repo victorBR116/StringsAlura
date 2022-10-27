66eb4c1c25c5d882398ae5e98d2496caf0a41896
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-10-27 16:44:51 -0300

    att

--- usuario.php
+++ usuario.php
@@ -1,8 +1,17 @@
 <?php
 
-$email = "Victor@gmail.com";
+$email = "Víctor@gmail.com";
+$senha = "ççççç";
+
+echo mb_strlen($senha) . PHP_EOL;
+
+if (mb_strlen($senha) < 8){
+    echo "Senha fraca" . PHP_EOL;
+}
 
 $posicaoDoArroba = strpos($email, '@');
 
-echo substr($email, 0, $posicaoDoArroba) . PHP_EOL;
+$usuario = substr($email, 0, $posicaoDoArroba);
+
+echo mb_strtoupper($usuario) . PHP_EOL;
 echo substr($email, $posicaoDoArroba + 1) . PHP_EOL;
