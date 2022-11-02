0c6cd78db8bd21a74c05d4c71809846b8d712386
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-11-02 14:08:19 -0300

    att

---
+++ documentacao.php
@@ -0,0 +1,7 @@
+<?php
+
+$trans = ["hello" => "hi", "hi" => "Hello"];
+
+echo strtr("hi all, I said hello", $trans) . PHP_EOL;
+
+echo str_replace(['hello', 'hi'], ['hi, hello'], "hi all, I said hello") . PHP_EOL;
---
+++ html.php
@@ -0,0 +1,6 @@
+<?php
+$nome = "Victor";
+
+?>
+
+<input type="text" name = "<?php echo addslashes($nome); ?>">
