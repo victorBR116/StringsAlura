1b6d9da8626b40b937f131b6961433e91197fdc7
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-11-04 16:55:52 -0300

    att

--- regex.php
+++ regex.php
@@ -1,18 +1,25 @@
 <?php
 
-$telefone = ['sad (11) 99577 - 4642', '(22) 98298 - 9239 sada', '(72) 93293 - 9238'];
+$telefone = ['(11) 99577 - 4642', '(22) 98298 - 9239', '(72) 93293 - 9238'];
 
 foreach ($telefone as $telefone){
+    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/';
     $telefoneValido = preg_match(
-        '/\([0-9]{2}\) 9?[0-9]{4} - [0-9]{4}/', 
+        $regex, 
         $telefone,
         $correspondecias
     );
 
-    var_dump($correspondecias);
+    //var_dump($correspondecias);
     if ($telefoneValido){
         echo 'Telefone valido' . PHP_EOL;
     }else{
         echo 'Telefone inválido' . PHP_EOL;
     }
+
+   echo preg_replace(
+        $regex,
+        '(XX) \2',
+        $telefone
+    ) . PHP_EOL;
 }
